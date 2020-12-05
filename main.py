13759360f94bab0e0790393e4c4a23facc27afc8
Michael Nemni <michael.nemni@gmail.com>
2020-12-05 18:35:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import track_features as tf
 from to_db import commit_db, add_artist, check_artist
 import graph_gen
 
-artist_name = input('Input artist name: ')
+artist_name = input('Input artist name: ').title()
 
-# If artist not in artist table, update
 artist_id = check_artist(artist_name)
 if artist_id == None:
     add_artist(artist_name)
@@ -17,7 +16,12 @@ else:
 
     commit_db(track_info_list, artist_id[0])
 
-graph_gen.get_data(artist_id[0])
+try:
+    graph_gen.get_data(artist_id[0], artist_name)
+except:
+    print("This is a new artist.\nArtist added to database, please try again")
+
+
 
 
 
